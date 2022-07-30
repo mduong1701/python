f23d936dcf1a41d1ad26a6dcbe7ea978eb1d5e3f
mduong1701 <minhmanlk0411@gmail.com>
2022-07-29 23:44:54 -0700

    Added the first python file

---
+++ fundamentals/introduction/testing_python.py
@@ -0,0 +1,19 @@
+import random
+
+print('Welcome to Python!')
+
+print('This is a loop printing 5 times')
+for x in range(1, 6):
+    print(f'x is: {x}')
+
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+day = random.choice(weekdays)
+
+print(f'Today is: {day}')
+
+if day == 'Monday':
+    print('It will be a long week!')
+elif(day == 'Friday'):
+    print('Woohoo, time for the weekend!')
+else:
+    print('Not quite there yet.')
