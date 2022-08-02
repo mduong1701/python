f799e67681379a0dab97c0128165709b779ee291
mduong1701 <minhmanlk0411@gmail.com>
2022-08-01 18:22:58 -0700

    Added dojo pets project

---
+++ 08.01/dojo_pets.py
@@ -0,0 +1,47 @@
+class Ninja:
+    def __init__(self, first_name, last_name, pet, treats, pet_food):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pet = pet
+        self.treats = treats
+        self.pet_food = pet_food
+
+    def walk(self):
+        self.pet.play()
+
+    def feed(self):
+        self.pet.eat()
+
+    def bathe(self):
+        self.pet.noise()
+
+    def display(self):
+        print(self.pet.health)
+        print(self.pet.energy)
+
+class Pet:
+    def __init__(self, name, type, tricks, health, energy):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.health = health
+        self.energy = energy
+
+    def sleep(self):
+        self.energy += 25
+
+    def eat(self):
+        self.energy += 5
+        self.health += 10
+
+    def play(self):
+        self.health += 5
+
+    def noise(self):
+        print("Meow")
+
+man = Ninja("Mike", "Le", Pet("Sausage", "Pit Bull", ["jump"], 100, 100), "beef", "cereal")
+man.feed()
+man.walk()
+man.bathe()
+man.display()
