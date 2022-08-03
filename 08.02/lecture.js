f1d692fb41f87ed928bd16526a8370939a4e8b4f
Man Duong <mduong8911@hotmail.com>
2022-08-03 09:10:40 -0700

    Added lecture

---
+++ 08.02/lecture.js
@@ -0,0 +1,26 @@
+// str = "     Federal    Bureau     Investigation"
+// arr = str.split(" ")
+
+// result = ""
+// for (var i = 0; i < arr.length; i++){
+//     result += arr[i][0];
+// }
+// console.log(result.toUpperCase())
+
+// str = "object oriented programming"
+// result = str[0];
+// for (var i = 1; i < str.length; i++){
+//     if (str[i] === " "){
+//         result += str[i + 1];
+//     };
+// };
+// console.log(result.toUpperCase());
+
+// str = "     Federal    Bureau     Investigation"
+// result = ""
+// for (var i = 1; i < str.length; i++){
+//     if (str[i] != " " && str[i-1] == " "){
+//         result += str[i];
+//     }
+// }
+// console.log(result.toUpperCase());
