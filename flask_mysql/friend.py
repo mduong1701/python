87ca8e58140893e674e40ecec1aaa4172b8abef9
Man Duong <mduong8911@hotmail.com>
2022-08-11 09:04:47 -0700

    Added new files

---
+++ flask_mysql/friend.py
@@ -0,0 +1,25 @@
+# import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
+# model the class after the friend table from our database
+
+class Friend:
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.occupation = data['occupation']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    # Now we use class methods to query our database
+    # @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM friends;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL('first_flask').query_db(query)
+        # Create an empty list to append our instances of friends
+        friends = []
+        # Iterate over the db results and create instances of friends with cls.
+        for friend in results:
+            friends.append(cls(friend))
+        return friends
