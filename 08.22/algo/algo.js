94d1d416096046ec8de679e7a86a503151dc8dd2
Man Duong <mduong8911@hotmail.com>
2022-08-22 09:21:17 -0700

    Added 08.22 algo

---
+++ 08.22/algo/algo.js
@@ -0,0 +1,24 @@
+function maxArr(arr){
+    ans = [];
+    if (arr.length == 0){
+        return []
+    }
+    maxNum = arr[0]
+    for (var i = 1; i < arr.length; i++){
+        if (maxNum < arr[i]){
+            maxNum = arr[i]
+        }
+    }
+    for (var j = 0; j < arr.length; j++){
+        if (maxNum == arr[j]){
+            ans.push(j);
+        }
+    }
+    return ans;
+}
+
+console.log(maxArr([]));
+console.log(maxArr([1]));
+console.log(maxArr([1,5]));
+console.log(maxArr([5,2,5]));
+console.log(maxArr([8,3,8,4,8]));
